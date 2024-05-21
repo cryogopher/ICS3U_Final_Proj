24b1efa3b7237f50e9c15cbfe9242898fa6175f2
Daniel Lin <87847438+cryogopher@users.noreply.github.com>
2024-05-21 14:08:39 -0400

    Add files via upload

--- Binary Bomber/src/Main.java
+++ Binary Bomber/src/Main.java
@@ -6,5 +6,34 @@ import java.io.*;
 import java.awt.event.*;
 import java.nio.Buffer;
 import java.security.Key;
-public class Main {
+public class Main extends JPanel implements KeyListener{
+    // Game States
+    // 0 - Start
+    // 1 - High score/Credits
+    // 2 - Gameplay Screen
+    // 3 - Loss
+
+
+    //panel generating
+    public Main(){
+        setPreferredSize(new Dimension(400, 600));
+        this.setFocusable(true);
+        addKeyListener(this);
+    }
+
+    public static void main(String[] args) throws IOException{
+        JFrame frame = new JFrame("Binary Bomber");
+        JPanel panel = new Main();
+        frame.add(panel);
+        frame.setVisible(true);
+        frame.pack();
+        
+
+    }
+
+    public void keyPressed(KeyEvent e){}
+    public void keyReleased(KeyEvent e){}
+    public void keyTyped(KeyEvent e){}
+
+
 }
